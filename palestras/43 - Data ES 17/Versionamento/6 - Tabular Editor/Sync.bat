65ab3b7bd0ae3fd60f0075fdf1f9ffafa3716b96
unknown <regianelima.ads@gmail.com>
2026-06-27 12:20:03 -0300

    atualizacoes

--- palestras/43 - Data ES 17/Versionamento/6 - Tabular Editor/Sync.bat
+++
@@ -1,8 +0,0 @@
-cd teste-versionamento-tom
-git checkout -b development
-git fetch origin main
-git add .
-git commit -m "Daily Integration %date% - %time%"
-git pull --rebase origin main
-git push -f origin development
-pause
--- palestras/43 - Data ES 17/Versionamento/7 - Azure DevOps/Sync.bat
+++
@@ -1,8 +0,0 @@
-cd powerbi-versionamento
-git checkout -b development
-git fetch origin main
-git add .
-git commit -m "Daily Integration %date% - %time%"
-git pull --rebase origin main
-git push -f origin development
-pause
